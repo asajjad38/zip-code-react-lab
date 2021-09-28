df57286b4a1c1f5bad351b5054e9b2fac4942830
asajjad38 <asajjad94@gmail.com>
2021-09-28 17:55:30 -0400

    Enable zipcode search

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -3,25 +3,58 @@ import './App.css';
 
 
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (<div class="card" Style="width: 18rem">
+            <div class="card-header">
+            {props.cityDetails.LocationText}
+            </div>
+            <ul>
+              <li>State: {props.cityDetails.State}</li>
+              <li>Location: {props.cityDetails.Long}</li>
+              <li>Population (estimated): {props.cityDetails.EstimatedPopulation}</li>
+              <li>Total Wages: {props.cityDetails.TotalWages}</li>
+            </ul>
+          </div>);
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (<div>Zip Code: <input onChange= {props.changeHandler} type="text"></input></div>);
 }
 
 
 class App extends Component {
+
+  state = {
+    zipCode: '',
+    cities: []
+  }
+
+  zipChanged = (event) => {
+    console.log(event.target.value);
+    if (event.target.value.length === 5){
+      console.log("Valid Zipcode Length")
+      this.setState({zipCode : event.target.value})
+
+      fetch('http://ctp-zip-api.herokuapp.com/zip/'+event.target.value)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          this.setState({
+            cities : data
+          })
+        })
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField zipcode={this.state.zipCode} changeHandler={this.zipChanged}/>
         <div>
-          <City />
-          <City />
+          {this.state.cities.map(x => <City cityDetails={x}/>)}
         </div>
       </div>
     );
